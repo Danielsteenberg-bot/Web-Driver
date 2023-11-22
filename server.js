bba24ae1659402d45ceda2ee11f089c8125e5c0a
Richard Vullings <rvullings8@gmail.com>
2023-11-22 11:38:40 +0100

    Setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const express_session = require('express-session');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app)
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
