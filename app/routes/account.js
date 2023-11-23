2997bd7058f0f68c0d53fc4d298ba315ffd633de
Richard Vullings <rvullings8@gmail.com>
2023-11-23 13:06:01 +0100

    moved register code to  correct route folder

--- app/routes/account.js
+++ app/routes/account.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { validate } = require('../classes/user');
+const { validate, register } = require('../classes/user');
 const router = express.Router()
 const app = express()
+const parseUrl = require('body-parser');
+
+let encodeUrl = parseUrl.urlencoded({ extended: false });
 
 router.route('/login')
     .get((req, res) => {
@@ -13,12 +16,25 @@ router.route('/login')
         const data = req.body
         const user = await validate(data.login_username, data.login_password)
 
-        if(user){
+        if (user) {
             console.log(user);
         }
-        
+
     })
 
+router.route('/register')
+    .get(encodeUrl, (req, res) => {
+        res.render('register')
+    })
+    .post(encodeUrl, async (req, res) => {
+        const username = req.body.userName;
+        const email = req.body.email;
+        const password = req.body.passWord;
+
+        const user = await register(username, email, password)
+
+        console.log(user)
+    });
 
 
 module.exports = router
