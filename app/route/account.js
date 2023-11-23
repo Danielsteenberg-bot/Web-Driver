7d96b83f56276f84b2a0e0ef9cf2bf98b5fc0ec0
Kasper Korthsen <125364229+Kantarellen1@users.noreply.github.com>
2023-11-23 12:24:21 +0100

    Login and Register created

---
+++ app/route/account.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const sessions = require('express-session');
+var parseUrl = require('body-parser');
+const app = express.Router();
+
+
+
+var mySql = require ('mysql');
+const { test } = require('../classes/user');
+
+
+let encodeUrl = parseUrl.urlencoded({extended: false});
+
+app.get('/register', encodeUrl, (req, res) =>{
+    res.render('register')
+})
+
+app.post('/register', encodeUrl, async (req,res) => {
+    const username = req.body.userName;
+    const email = req.body.email;
+    const password = req.body.passWord;
+
+    const user = await test(username, email, password)
+
+    console.log(user)
+});
+
+module.exports = app        
--- app/server.js
+++ app/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const express_session = require('express-session');
 const http = require('http');
 const socketIO = require('socket.io');
-
+var parseUrl = require('body-parser');
+const res = require('express/lib/response');
+const cookieParser =("cookie-parser")
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
@@ -12,7 +15,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/', require('./route/account'))
+
 const port = 3000;
-server.listen(port, () => {
+server.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
 });
+
