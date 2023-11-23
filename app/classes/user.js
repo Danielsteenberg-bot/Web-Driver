dc285abea447058ac01c9a805ff02a020589af7d
Richard Vullings <rvullings8@gmail.com>
2023-11-23 10:29:50 +0100

    Login working

---
+++ app/classes/user.js
@@ -0,0 +1,30 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient()
+const bcrypt = require('bcrypt');
+
+const hashPassword = async (pass) => await bcrypt.hash(pass, 10)
+const comparePassword = async (pass, hash) => await bcrypt.compare(pass, hash)
+
+const validate = async (loginName, password) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            OR:[
+                {email: loginName},
+                {username: loginName}
+            ]
+        }
+    })
+    if (user) {
+            let result = await comparePassword(password, user.password)
+            if(!user) return false
+
+            return user
+    }
+
+    return false
+} 
+
+
+module.exports = {
+    validate
+}
--- app/server.js
+++ app/server.js
@@ -12,6 +12,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+//Routers
+app.use('/account', require('./routes/account'))
+
 const port = 3000;
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
