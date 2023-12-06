0d56f09edb8511eaae84576544628bff4048f13b
Kasper Korthsen <125364229+Kantarellen1@users.noreply.github.com>
2023-12-06 13:31:06 +0100

    Created Cordinate table and cleaned up some stuff

--- app/classes/driving_history.js
+++ app/classes/driving_history.js
@@ -1,23 +1,8 @@
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient()
 const express = require('express');
-const {Server} = require('socket.io')
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
-const io = new Server(server);
-
-const listener = (driving) => {
-    console.log(driving);
-}
-
-socket.on("forward", "left", "right", "backwards", listener);
-
-const driver = async(forward, left, right, backwards) => {
-    const drivingData = await sessionLog(forward)
-}
-
-
 
 const register = async(sessionID, distance, angle, battery) => {
     const sessionData = await sessionLog(sessionID)
@@ -40,6 +25,5 @@ const register = async(sessionID, distance, angle, battery) => {
     }
 
     module.exports = {
-        driver,
         register
     }
