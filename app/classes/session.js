951067ba56131c95d6d7d6e655b4f91be37810ac
Richard Vullings <rvullings8@gmail.com>
2023-12-11 13:03:25 +0100

    direction + rooms fixed + joins fixed

--- app/classes/session.js
+++ app/classes/session.js
@@ -1,22 +1,22 @@
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient()
 
-const test = async (user, direction) => {
-    const session = await prisma.session.create({
-        data:{
-            userid: user,
-            direction
-        },
-        select: {
-            id: true,
-            direction: true,
-            userid: true
-        }
-    })
+// const test = async (user, direction) => {
+//     const session = await prisma.session.create({
+//         data:{
+//             userid: user,
+//             direction
+//         },
+//         select: {
+//             id: true,
+//             direction: true,
+//             userid: true
+//         }
+//     })
 
-    return session
-}
+//     return session
+// }
 
-module.exports = {
-    test
-}
+// module.exports = {
+//     test
+// }
