41531d51d75f10984644fc9a43908368137a3bc6
0-Mqix <maxvanmoorsel2@gmail.com>
2023-12-14 09:16:04 +0100

    fix before merge conflicts

--- device/settings.h
+++ device/settings.h
@@ -1,6 +1,21 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#define CMPS11_ADDRESS 0x60
+#define DISABLE_CMPS11 0
+
+#define GPS_PPS_PIN 2
+
+#define DRIVE_ACTION_DURATION 500
+#define DRIVE_TO_SIDE_SWITCH_TIME 250
+
+#define WIFI_CLIENT true
+
+#define PROXY_IP IPAddress(192, 168, 137, 1)
+#define PROXY_PORT 8888
+#define WIFI_SSID "jungle-network"
+#define WIFI_PASSWORD "83:511Nx"
+
 #define DEVICE_ID 1
 #define AP_PASSWORD "test" 
 
